6d5106c1dd7c834ace2aa2c719c7a333d6348fbd
Meshack Korir <meshackkorir100@gmail.com>
2023-06-22 12:02:34 +0300

    cretaed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,16 +9,12 @@ class BirdsController < ApplicationController
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+    render json: bird
     end
 
     def create
-      bird = Bird.create(name: ???, species: ???)
+      bird = Bird.create(name: params[:name], species: params[:species])
       render json: bird, status: :created
     end  
   end
 
-end
